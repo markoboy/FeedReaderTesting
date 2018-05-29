5ef4fd03c9a2083e0294d53d15483c3d4f2d6ed8
Athanasios Markou <thanasismarko@gmail.com>
2018-05-29 10:49:30 +0100

    feat: Add a test to ensure allFeeds have name defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -42,6 +42,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('have a defined name', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            });
+         });
     });
 
 
